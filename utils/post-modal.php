e57e1b4f6b3ebe3b64fe20df2209815d17a10969
kylian facquet <kylianfacquet@gmail.com>
2025-04-13 13:44:25 +0200

    Ajout de la description dans le modal

--- utils/post-modal.php
+++ utils/post-modal.php
@@ -21,11 +21,45 @@
     ?>
     <div class="modal-close" data-dismiss="post">&times;</div>
     <div class="modal-header">
-        <p><?php echo $image->titre;?></p>
+        <div class="title">
+            <p><?php echo $image->titre;?></p>
+        </div>
+        <div class="description">
+            <p><?php echo $image->desc;?></p>
+        </div>
+        <div class="author">
+
+        </div>
+        <div class="category">
+
+        </div>
+        <div class="tags">
+            
+        </div>
     </div>
     <div class="modal-body">
-        <img src="<?= htmlspecialchars($image->source) ?>" alt="Image">
-
+        <div class="img">
+            <img src="<?= htmlspecialchars($image->source) ?>" alt="Image">
+        </div>
+        <div class="comments">
+            <h3>Commentaires</h3>
+            <div class="comment-list">
+                <?php
+                    // $comments = get_comments_from_image($connexion, $id);
+                    // foreach($comments as $comment){
+                    //     echo "<div class='comment'>";
+                    //     echo "<p class='comment-author'>".$comment->auteur."</p>";
+                    //     echo "<p class='comment-content'>".$comment->contenu."</p>";
+                    //     echo "</div>";
+                    // }
+                ?>
+            </div>
+            <form action="#" method="post" class="comment-form">
+                <input type="text" name="author" placeholder="Votre nom" required>
+                <textarea name="content" placeholder="Votre commentaire" required></textarea>
+                <button type="submit">Ajouter un commentaire</button>
+            </form>
+        </div>
     </div>
     <div class="modal-footer">
         <a href="#" class="btn btn-close" role="button" data-dismiss="post">fermer</a>
